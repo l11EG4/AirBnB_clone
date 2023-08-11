9a858c898cbc1ec2242ecdb5bc9cec8679d1584e
Laila <lailatabourit@gmail.com>
2023-08-11 14:28:57 -0700

    ALX_tasks

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines the entry point of the command interpreter."""
+import cmd
+import re
+from models.base_model import BaseModel
+from models import storage
+
+class HBNBCommand(cmd.Cmd):
+    """ Command interpreter class."""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Command Quit to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF to exit the program."""
+        print()
+        return True
+
+    def emptyline(self):
+        """ Empty line and do nothing."""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
