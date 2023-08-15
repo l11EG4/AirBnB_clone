57bf0adbff6f2d71a70b26c53dc0cbb9db66fb54
Laila <lailatabourit@gmail.com>
2023-08-15 06:01:57 -0700

    ALX_tasks

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel:
         Update public instace attribute with updated_at with
         the current datetime and save the instance to storage.
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
